b9596829e434238521a0fd3be57d53c893719ef1
Nick Matecki <nmatecki1@gmail.com>
2020-03-01 17:14:05 -0600

    Updates

---
+++ precache-manifest.7bbb07873d3fc356e25859b39aa81911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a61c4f2a7b87e10057a048423d908f9",
+    "url": "/SprayboothV1/index.html"
+  },
+  {
+    "revision": "8b92c8f5e07f6b1bc7a6",
+    "url": "/SprayboothV1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a6f16de4bffc3f48185c",
+    "url": "/SprayboothV1/static/js/2.0dedce7f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/SprayboothV1/static/js/2.0dedce7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b92c8f5e07f6b1bc7a6",
+    "url": "/SprayboothV1/static/js/main.1a423d0e.chunk.js"
+  },
+  {
+    "revision": "6a50d1748b4d0ade0d3c",
+    "url": "/SprayboothV1/static/js/runtime-main.b6422cb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SprayboothV1/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nmatecki/Spraybooth/precache-manifest.ae18c5f1bc43e8427b45c908a3e53b48.js"
+  "/SprayboothV1/precache-manifest.7bbb07873d3fc356e25859b39aa81911.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nmatecki/Spraybooth/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SprayboothV1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
